303b1dcf282c5db015110bebd5014dfa8208ab32
andreasbrownethz <91591097+andreasbrownethz@users.noreply.github.com>
2021-10-27 19:15:00 +0200

    Adding overall accuracy to the metrics

--- cmat3x3.py
+++ cmat3x3.py
@@ -18,9 +18,12 @@ def calc_f1(TP, FP, FN):
 #Assuming  that input matrices are always of size 3 x 3
 def compute_metrics(cmat): 
     
+    cmatsum = np.sum(cmat)
+    
     TP0 = cmat[0, 0]
     FP0 = cmat[1, 0] + cmat[2, 0]
     FN0 = cmat[0, 1] + cmat[0, 2]
+    TN0 = cmatsum - (TP0 + FP0 + FN0)
     precision0 = calc_precision(TP0, FP0)
     recall0 = calc_recall(TP0, FN0)
     f10 = calc_f1(TP0, FP0, FN0)
@@ -28,6 +31,7 @@ def compute_metrics(cmat):
     TP1 = cmat[1, 1]
     FP1 = cmat[0, 1] + cmat[2, 1]
     FN1 = cmat[1, 0] + cmat[1, 2]
+    TN1 = cmatsum - (TP1 + FP1 + FN1)
     precision1 = calc_precision(TP1, FP1)
     recall1 = calc_recall(TP1, FN1)
     f11 = calc_f1(TP1, FP1, FN1)
@@ -35,6 +39,7 @@ def compute_metrics(cmat):
     TP2 = cmat[2, 2]
     FP2 = cmat[0, 2] + cmat[1, 2]
     FN2 = cmat[2, 0] + cmat[2, 1]
+    TN2 = cmatsum - (TP2 + FP2 + FN2)
     precision2 = calc_precision(TP2, FP2)
     recall2 = calc_recall(TP2, FN2)
     f12 = calc_f1(TP2, FP2, FN2)
@@ -43,7 +48,13 @@ def compute_metrics(cmat):
     recalls = np.array([recall0, recall1, recall2])
     f1s = np.array([f10, f11, f12])
     
-    return precisions, recalls, f1s
+    sumTP = TP0 + TP1 + TP2
+    sumFP = FP0 + FP1 + FP2
+    sumFN = FN0 + FN1 + FN2
+    sumTN = TN0 + TN1 + TN2
+    overall_accuracy = (sumTP + sumTN)/(sumTP + sumFP + sumFN + sumTN)
+    
+    return overall_accuracy, precisions, recalls, f1s
     
 #Example
 confusionmat = np.array([[150,21,2, 17], [12,730,1, 5], [5,0,83,0], [25,30,4, 350]])
@@ -51,7 +62,9 @@ confusionmat = confusionmat = np.transpose(confusionmat)
 cmsnip = confusionmat[:3,:3]
 print(cmsnip)
 
-precisions, recalls, f1s = compute_metrics(cmsnip)
+overall_accuracy, precisions, recalls, f1s = compute_metrics(cmsnip)
+print(overall_accuracy)
+print()
 print(precisions)
 print()
 print(recalls)
