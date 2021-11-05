893755b8cde4c23dd4309865e23927742503c6bb
andreasbrownethz <91591097+andreasbrownethz@users.noreply.github.com>
2021-11-05 14:41:21 +0100

    Functions to calculate RMSE and MAE

---
+++ Lab02/code/RMSE_MAE.py
@@ -0,0 +1,37 @@
+import numpy as np
+import math
+
+#A function to compute root mean square error (RMSE)
+def calculate_RMSE(GT, Pred):
+    if np.shape(GroundTruth) == np.shape(Predictions): #First check that the inputs are compatible
+        N = np.shape(GroundTruth)[0] * np.shape(GroundTruth)[1]  #Total number of elements or pixels being compared
+        RMSE = np.sqrt((1/N)*(np.sum((Predictions - GroundTruth)**2))) #RMSE calculation
+        return RMSE
+    else:
+        print('Error: Incompatible Matrices')
+        
+#A function to compute mean absolute error (MAE)
+def calculate_MAE(GT, Pred):
+    if np.shape(GroundTruth) == np.shape(Predictions): #First check that the inputs are compatible
+        N = np.shape(GroundTruth)[0] * np.shape(GroundTruth)[1]  #Total number of elements or pixels being compared
+        MAE = (1/N)*(np.sum(np.abs(Predictions - GroundTruth))) #MAE calculation
+        return MAE
+    else:
+        print('Error: Incompatible Matrices')
+
+GroundTruth = np.array([[1, 9], [6, 8]])
+print(GroundTruth)
+print('')
+Predictions = np.array([[5, 2], [4, 3]])
+print(Predictions)
+print('')
+
+RMSE = calculate_RMSE(GroundTruth, Predictions)
+print('RMSE')
+print(RMSE)
+print('')
+
+MAE = calculate_MAE(GroundTruth, Predictions)
+print('MAE')
+print(MAE)
+print('')
