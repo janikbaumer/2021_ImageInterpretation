d2468a64a3701df3c1f2bf9ebb847eb4dd831e88
ferdal <ferdal@student.ethz.ch>
2021-11-17 01:29:28 +0100

    Faen ta hele kloden

--- Lab02/code/main_Features.py
+++ Lab02/code/main_Features.py
@@ -8,6 +8,7 @@ import os
 import cv2
 import pickle as cPickle
 import platform
+import sympy
 
 from tqdm import tqdm
 from torchvision.datasets.vision import VisionDataset
@@ -18,22 +19,40 @@ from sklearn.metrics import f1_score, cohen_kappa_score
 from sklearn.preprocessing import StandardScaler
 from scipy import ndimage
 from datetime import datetime
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input
+#from tensorflow.keras.applications.vgg16 import VGG16
+#from tensorflow.keras.preprocessing import image
+#from tensorflow.keras.applications.vgg16 import preprocess_input
 import tensorflow as tf
-from tensorflow import keras
+#from tensorflow import keras
 
 from numpy import ma
 
 # %%
 ### CLASSES
 
-from SatelliteSet import SatelliteSet
+#from SatelliteSet import SatelliteSet
 
 # %%
 ### FUNCTIONS
+def dp(n, left): # returns tuple (cost, [factors])
+    if (n, left) in memo: return memo[(n, left)]
 
+    if left == 1:
+        return (n, [n])
+
+    i = 2
+    best = n
+    bestTuple = [n]
+    while i * i <= n:
+        if n % i == 0:
+            rem = dp(n / i, left - 1)
+            if rem[0] + i < best:
+                best = rem[0] + i
+                bestTuple = [i] + rem[1]
+        i += 1
+
+    memo[(n, left)] = (best, bestTuple)
+    return memo[(n, left)]
 # %%
 ### INITIALIZING
 # Defining variables for future use
@@ -67,8 +86,8 @@ elif current_os == 'Windows':
 
 
 DATA_SET = "train"
-
 DATA_FOLDER = cwd+sep+'datasets'+sep+DATA_SET
+
 
 # %% Data Loading
 if __name__ == "__main__":
@@ -79,32 +98,91 @@ if __name__ == "__main__":
     # create datasets
     #dset = SatelliteSet(root=DATA_FILE, windowsize=windSize, test=False)
 
-    # TODO: Load the pretrained-NN here, modify parameters
-    model = tf.keras.applications.EfficientNetB0(
-        include_top=False,
-        weights="imagenet",
-        input_tensor=None,
-        input_shape=(10980, 10980, 4),
-        pooling="avg"
-    )
 
-    directory = os.fsencode(DATA_FOLDER)
+
+    directory = DATA_FOLDER
 
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        if filename.endswith(".h5"):
+        if filename.endswith(".npy"):
 
             # Load Image
+            """
             dset = h5py.File(filename, 'r')
             NIR = dset['NIR']
             RGB = dset['RGB']
             Y = dset['GT']
             X = np.concatenate([RGB, np.expand_dims(NIR, axis=-1)], axis=-1)
-            model.input_shape = np.shape(X)
+            """
+            X = np.load(DATA_FOLDER+sep+filename)
+            print(np.isnan(X))
+            print(np.shape(X))
+
+            #plt.imshow(X_masked)
+            #plt.show()
+
+            meanRNIR = np.nanmean(np.array([X[:, :, 0], X[:, :, 3]]), axis=0)
+            print(meanRNIR)
+            print('')
+            meanGNIR = np.nanmean(np.array([X[:, :, 1], X[:, :, 3]]), axis=0)
+            print(meanGNIR)
+            print('')
+            meanBNIR = np.nanmean(np.array([X[:, :, 2], X[:, :, 3]]), axis=0)
+            print(meanBNIR)
+
+            X = np.stack([meanRNIR, meanGNIR, meanBNIR], axis=-1)
+            print(X)
+
+            mask = np.isnan(X[:, :, 0])
+            mean01 = np.mean(X[:, :, 0][np.logical_not(mask)])
+            mean02 = np.mean(X[:, :, 1][np.logical_not(mask)])
+            mean03 = np.mean(X[:, :, 2][np.logical_not(mask)])
+
+
+            RNIR = X[:,:,0]
+            GNIR = X[:, :, 1]
+            BNIR = X[:, :, 2]
+
+            RNIR[mask] = mean01
+            GNIR[mask] = mean02
+            BNIR[mask] = mean03
+
+            X = np.stack([RNIR, GNIR, BNIR], axis=-1)
+            print(X)
+
+            X_masked = X
+            """
+            # TODO: Apply masking
+            mask = np.isnan(X)
+            X_masked = X[np.logical_not(mask)]
+            print(np.shape(X_masked))
+            n = np.shape(X_masked)
+            n = n[0]/3
+            if sympy.isprime(n)
+                X_masked.drop[]
+
+            memo={}
+            m = dp(int(n), 2)
+            print(m)
+            """
+
+
+
+
+            # TODO: Load the pretrained-NN here, modify parameters
+            model = tf.keras.applications.EfficientNetB0(
+                include_top=False,
+                weights="imagenet",
+                input_tensor=None,
+                input_shape=np.shape(X_masked),
+                pooling="avg"
+            )
+
+            # TODO: Reshape X_masked to right size, exactly 3 channels
 
             # TODO: Do we do feature extraction on the whole image at once? LETS TRY
             print('STARTING FEATURE EXTRACTION ...')
-            features = model.predict(X)  # Returns the features as a np array.
+            features = model.predict(X_masked) # Returns the features as a np array.
             # This still needs to be modified to have the parameters. See documentation. For example, predict with batches or not?
 
             # TODO: Here the feature vectors should be stored and saved, in order to retrieve it later for the training.
@@ -113,11 +191,11 @@ if __name__ == "__main__":
             #   every batch has different feature vector lengths, because of varying number of good pixels. Solution might
             #   be to add the mask before training.
 
-            FEAT_FILE = DATA_FOLDER+cwd+sep+filename[0:-3]+'_features.h5'
+            FEAT_FILE = DATA_FOLDER+cwd+sep+filename[0:-4]+'_features.h5'
             h5f_feat = h5py.File(FEAT_FILE, 'w')
             h5f_feat.create_dataset('feature_vectors', data=features)
             h5f_feat.close()
-            dset.close()
+            #dset.close()
             continue
         else:
             continue
