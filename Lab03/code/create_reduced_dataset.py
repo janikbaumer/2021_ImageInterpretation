d5f547c221ef9763756e45aee0e02efdf5b7e6c9
Janik Baumer <janik.baumer@gmail.com>
2021-11-30 16:18:28 +0100

    create smaller dataset for development

---
+++ Lab03/code/create_reduced_dataset.py
@@ -0,0 +1,18 @@
+import h5py
+import numpy
+
+PATH_OLD = '../data/imgint_trainset.hdf5'
+PATH_NEW = '../data/imgint_trainset_reduced.hdf5'
+
+file_old = h5py.File(PATH_OLD, 'r')
+# for checking (later) that dimensions match (in new dataset)
+data_old = file_old['data']
+gt_old = file_old['gt']
+
+data_new = file_old['data'][0:500, :, :]
+gt_new = file_old['gt'][0:500]
+
+with h5py.File(PATH_NEW, "w") as f:
+    dset1 = f.create_dataset('data', data=data_new)
+    dset2 = f.create_dataset('gt', data=gt_new)
+
